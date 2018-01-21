03486efe42a1b779c396e561216e1a0f0b6fa06e
kaiwen2times <kxz6582@rit.edu>
2018-01-20 23:11:06 -0500

    working on knn implementation.

--- hw4/sigmoid.py
+++ hw4/sigmoid.py
@@ -1,9 +1,10 @@
 import numpy as np
+import pdb
 
 def sigmoid(z):
     # sigmoid Compute sigmoid functoon
     # g = sigmoid(z) computes the sigmoid of z.
-
-    g = 1 / (1 + np.exp(-z));
+    pdb.set_trace()
+    g = 1 / (1 + np.exp(-z))
 
     return g
